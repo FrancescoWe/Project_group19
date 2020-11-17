521bd2b90d888e72ba9f8ea5e4a547a660a248bd
Francesco Weikmann <francesco.weikmann@studenti.unitn.it>
2020-11-17 15:39:47 +0100

    Aggiunto connession a database online

--- group_19/app.js
+++ group_19/app.js
@@ -6,6 +6,7 @@ require ('dotenv').config();
 const request = require('request');
 const meteoUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Trento,it&units=metric&appid='+ process.env.API_KEY;
 
+const users = require('./api/routes/users');
 //routes constant
 const userRoutes = require('./api/routes/users');
 const meteoRoutes = require('./api/routes/meteos');
@@ -17,8 +18,10 @@ app.use('/users', userRoutes);
 app.use('/meteos', meteoRoutes);
 app.use(express.static(__dirname + "/"));
 
-//connecting to db
-mongoose.connect('mongodb://localhost/testaroo',{
+app.use('/api/v1/users', users);
+
+//connecting to db offline
+/*mongoose.connect('mongodb://localhost/testaroo',{
   useUnifiedTopology: true,
   useNewUrlParser:true
 });
@@ -27,7 +30,7 @@ const db = mongoose.connection;
 db.once('open', () => {
     console.log('DB connected successfully!');
   });
-
+*/
 //using request for weather api from openweather
 
 //faccio una richiesta allo url e una callback function che prende dei parametri
@@ -35,5 +38,14 @@ db.once('open', () => {
 const data = JSON.parse(body);
  console.log(data);
 })*/
+
+//connessione al db online
+mongoose.connect(process.env.DB_CONNECTION_ONLINE,{
+  useUnifiedTopology: true,
+  useNewUrlParser:true
+}).then(()=>{
+  console.log("CONNESSO ONLINE")
+});
+
 
 module.exports=app;
