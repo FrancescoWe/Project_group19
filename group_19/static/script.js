7a9ee6b4923b1557a53269756d3ccde6e8d7f8d0
Giacomo Catelan <giacomo.catelan@studenti.unitn.it>
2020-11-18 13:17:18 +0100

    Modifica parziale index.html per verifica del corretto funzionamento di forecast API call - MODIFICARE LOGICA ORARIO

--- group_19/static/script.js
+++ group_19/static/script.js
@@ -1,4 +1,15 @@
 var loggedUser;
+var daycount=0;
+var searchbefore = "";
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "",
+    "method": "GET"
+}
+
+document.getElementById('daysbutton').style.visibility="hidden";
 
 function login(){
     //var email = document
@@ -21,31 +32,92 @@ function login(){
 }
 
 function waitForElement(){
+    document.getElementById('daysbutton').style.visibility="hidden";
+    document.getElementById('daynum').style.visibility="hidden";
+    document.getElementById('hours').style.visibility="hidden";
+    daycount=0;
     var search = $('#searchbar').val();
     console.log(search);
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "./meteos/current/"+search,
-        "method": "GET"
-    }
+
+    settings.url = "./meteos/current/"+search;
 
     $.ajax(settings).done(function (response) {
         console.log(response);
 
+        document.getElementById('daynum').innerHTML = "";
+
         var name= response.name;		//prende la response name dal DB e la salva nella variabile name
-        $("#name").append(name);
+        document.getElementById('name').innerHTML = "City name : "+search;
 
         var content = response.wind.speed;	//prende la response wind.speed dal DB e la salva nella variabile content
-            $("#windSpeed").append(content);
+        document.getElementById('windSpeed').innerHTML = "Wind speed: "+content;
 
         var currentWeather = response.weather[0].main;	//prende la response weather[0] cioè il tempo attuale dal DB e la salva nella variabile currentweather
-            $("#currentWeather").append(currentWeather);
+        document.getElementById('currentWeather').innerHTML = "Current weather conditions : "+currentWeather;
 
+        /*
         $("#form").submit( function(res) {			//crea una variabile form che prende il risultato del submit di un bottone e lo salva dentro alla variabile search
             var search = $('#searchbar').val();
             // use the 'search' variable as needed here...
             // $.post('url...', { searchTerm: search });
         });
+        */
     });
+}
+
+function waitForElementForecast(){
+    document.getElementById('daynum').style.visibility="visible";
+    document.getElementById('hours').style.visibility="visible";
+    if(daycount>39){
+        document.getElementById('daysbutton').style.visibility="hidden";
+        daycount=0;
+        return;
+    } else {
+        document.getElementById('daysbutton').style.visibility="visible";
+        document.getElementById('daysbutton').innerHTML = "+1 Days";
+    }
+    
+    var search = $('#searchbar').val();
+
+    if((daycount/8)>=1 && search!==searchbefore){
+        document.getElementById('daynum').innerHTML = "City name changed";
+        document.getElementById('daysbutton').style.visibility="hidden";
+        return;
+    }
+    searchbefore = $('#searchbar').val();
+
+    settings.url = "./meteos/forecast/"+search;
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        document.getElementById('daynum').innerHTML = ((daycount/8)+1)+" days from now";
+
+        var hours = String(response.list[daycount].dt_txt).substring(11,13);
+        document.getElementById('hours').innerHTML = hours + ":00:00";
+
+        console.log(hours);
+
+        var name= response.city.name;		//prende la response name dal DB e la salva nella variabile name
+        document.getElementById('name').innerHTML = "City name : "+search;
+
+        var forecastwind = response.list[daycount].wind.speed;
+        document.getElementById('windSpeed').innerHTML = "Wind speed: : "+forecastwind;
+
+        var forecastWeather = response.list[daycount].weather[0].main;	//prende la response weather[0] cioè il tempo attuale dal DB e la salva nella variabile currentweather
+        document.getElementById('currentWeather').innerHTML = "Weather : "+forecastWeather;
+
+        /*
+        $("#form").submit( function(res) {			//crea una variabile form che prende il risultato del submit di un bottone e lo salva dentro alla variabile search
+            var search = $('#searchbar').val();
+            // use the 'search' variable as needed here...
+            // $.post('url...', { searchTerm: search });
+        });
+        */
+
+    });
+}
+
+function addday(){
+    daycount = daycount+8;
 }
