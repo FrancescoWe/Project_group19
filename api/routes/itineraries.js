e8a89314edfdd20e71e7231f8348b765038c94ce
Giacomo Catelan <giacomo.catelan@studenti.unitn.it>
2020-11-23 22:14:58 +0100

    Uso delle query di mongodb migliorato, snellimento nelle funzioni get, post e delete dell' API. Rimozione di molti cicli for inutili.

--- api/routes/itineraries.js
+++ api/routes/itineraries.js
@@ -5,7 +5,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const request = require('request');
-const MeteoComponent = require('../models/meteoComponent');
 const Itinerary = require('../models/itinerary');
 const User = require('../models/user');
 
@@ -16,102 +15,52 @@ const db = mongoose.connection;
 // Definizione del metodo GET: ricerca gli itinerari di tutti gli user
 router.get('/', async(req,res) => {
     try{
+
         let founduser = await User.findById(req.body.user_id);
-        if(founduser != undefined)
-            res.status(200).json(founduser.itinerary);
-        else
-            res.status(400).send("User with id: "+req.body.user_id+" not found");
+        res.status(200).json(founduser.itinerary);
+
     } catch(err){
-        res.status(400).send(err);
+        res.status(400).send("User with id: "+req.body.user_id+" not found");
     }
 });
 
 /* Definizione del metodo POST: crea un itinerario vuoto e lo salva,
 connettendolo ad un utente singolo (specifica l'id dell' utente nel body della post) */
-router.post('', async (req, res) => {
+router.post('/', async (req, res) => {
 
     try{
         let userfound = await User.findOne({_id: req.body.user_id});
-        //console.log("User with id: "+req.body.user_id+" found");
 
         let newitinerary = new Itinerary();
 
         await User.updateOne(
-            {_id: userfound},
-            {$push : {itinerary: newitinerary}}
+            {"_id" : userfound._id},
+            {"$push" : {"itinerary" : newitinerary}}
         );
 
-        //console.log('Itinerary saved and binded successfully to user '+userfound._id);
-        res.status(201).send('Itinerary saved and binded successfully to user '+userfound._id);
+        res.status(201).send('Itinerary saved and binded successfully to user '+req.body.user_id);
     }catch{
-        res.status(400).send("User with id: "+ req.body.id +" not found");
-        console.log("User with id: "+req.body.id+" not found");
+        res.status(400).send("User with id: "+ req.body.user_id +" not found");
     }
 
 });
 
 /* Definizione del metodo DELETE: elimina un determinato itinerario dal DB e dalla lista degli itinerari dell'utente che lo aveva.
 Richiede un oggetto JSON nel body della richiesta con il campo "id" dell'itinerario che si intende eliminare*/
-router.delete('', async (req,res)=> {
+router.delete('/', async (req,res)=> {
 
     try{
-        var usertomodify = await User.findById(req.body.user_id);
-        var itinerarytomodify;
-        var count=-1;
 
-        var infolenIT = Object.keys(usertomodify.itinerary).length;
-
-        for(let i=0;i<infolenIT;i++){
-            if(usertomodify.itinerary[i].id == req.body.itinerary_id){
-                itinerarytomodify = usertomodify.itinerary[i];
-                count=i;
-                infolenIT=-1;
-            }
-        }
-
-        if(count!=-1){
-            await User.updateOne(
-                { _id: req.body.user_id},
-                { $pull:  {itinerary : itinerarytomodify} }
-            ); 
-            res.status(201).send("Itinerary "+req.body.itinerary_id+" deleted\nUser "+usertomodify.id+" updated.\n");
-        } else {
-            res.status(400).send("Itinerary "+req.body.itinerary_id+" not found.\n");
-
-        } 
+        await User.updateOne(
+            { "_id": req.body.user_id},
+            { "$pull" :  {"itinerary" : {"_id" : req.body.itinerary_id} } }
+        );
+        
+        res.status(201).send("Itinerary "+req.body.itinerary_id+" deleted\nUser "+req.body.user_id+" updated.\n");
     }catch(err){
         res.status(400).send("Itinerary "+req.body.itinerary_id+" not found.\n");
     }
 
 });
-
-/*
-router.delete('', async (req,res)=> {
-
-    try{
-        let removedItinerary = await Itinerary.deleteOne({_id: req.body.itId});
-        res.send("Itinerary with id "+req.body.itId+" successfully deleted.");
-        console.log("Itinerary with id "+req.body.itId+" successfully deleted.");
-    }catch(err){
-        res.send("Itinerary with id "+req.body.itId+" not found.");
-        console.log("Itinerary with id "+req.body.itId+" not found.");
-    }
-
-});
-*/
-
-
-/*
-router.delete('/', async (req,res)=> {
-
-    try{
-        const removeditinerary = await Itinerary.deleteOne({_id: req.body.itineraryId})
-        res.json(removeditinerary);
-    }catch(err){
-        res.json({message: err});
-    }
-
-})
-*/
 
 module.exports = router;
--- api/routes/meteoComponents.js
+++ api/routes/meteoComponents.js
@@ -13,39 +13,16 @@ const db = mongoose.connection;
 // Definizione del metodo GET: ricerca i meteoComponents di tutti gli user
 router.get('/', async(req,res) => {
     try{
-        let founduser = await User.findById(req.body.user_id);
-        let founditinerary;
+        
+        let founditinerary = await User.findOne(
+            {"_id": req.body.user_id},
+            { "itinerary" : {$elemMatch : {"_id" : req.body.itinerary_id}}}
+        );
 
-        let infolenIT = Object.keys(founduser.itinerary).length;
+        res.status(201).send(founditinerary.itinerary[0].meteos_dates);
 
-        for(let i=0;i<infolenIT;i++){
-            if(founduser.itinerary[i].id == req.body.itinerary_id){
-                founditinerary = founduser.itinerary[i];
-                infolenIT=-1;
-            }
-        }
-
-        let meteoComponents = founditinerary.meteos_dates;
-        res.status(201).send(meteoComponents);
     } catch(err){
         res.status(400).send("Si è verificato un errore.");
-    }
-});
-
-/* Definizione del metodo GET con path "/:id": ricerca di un componente meteo tramite id.
-Utile per avere informazioni su un meteoComponent dopo averne ottenuto l'ID da un itinerario di un utente*/
-router.get('/:id', async (req, res) => {
-    try {
-        let meteoComponent = await MeteoComponent.findById(req.params.id).exec();
-        res.status(201).json({
-            self: '/api/v1/meteoComponents/' + meteoComponent.id,
-            temp_Max: meteoComponent.temp_Max,
-            temp_Min: meteoComponent.temp_Min,
-            date: meteoComponent.date,
-            cityName: meteoComponent.cityName
-        });
-    } catch(err){
-        res.status(400).send("La meteoComponent con id: " + req.params.id + " non esiste.");
     }
 });
 
@@ -53,22 +30,6 @@ router.get('/:id', async (req, res) => {
 router.post('', async (req, res) => {
 
     try{
-        let founduser = await User.findById(req.body.user_id);
-        //console.log("Itinerary with id:"+req.body.id+" found");
-
-        let founditinerary;
-        var count=-1;
-
-        let infolenIT = Object.keys(founduser.itinerary).length;
-
-        for(let i=0;i<infolenIT;i++){
-            if(founduser.itinerary[i].id == req.body.itinerary_id){
-                founditinerary = founduser.itinerary[i];
-                infolenIT=-1;
-                count=i;
-            }
-        }
-
         let meteoComponents = new MeteoComponent({
             temp_Max : req.body.temp_Max,
             temp_Min : req.body.temp_Min,
@@ -76,17 +37,14 @@ router.post('', async (req, res) => {
             cityName : req.body.cityName
         });
 
-        if(count!=-1){
-            await User.updateOne(
-                {"_id": req.body.user_id, "itinerary._id" : req.body.itinerary_id},
-                {"$push" : { "itinerary.$.meteos_dates" : meteoComponents } },
-            );
-            console.log("Meteo data binded to itinerary "+req.body.itinerary_id);
-            res.status(201).send("I dati del meteo sono stati aggiunti all' itinerario con id: "+req.body.itinerary_id+"\ncollegato all' utente con id: "+req.body.user_id+"\n");    
-        } else
-            res.status(400).send("Itinerary with id:"+req.body.itinerary_id+" not found");
+        await User.updateOne(
+            {"_id": req.body.user_id, "itinerary._id" : req.body.itinerary_id},
+            {"$push" : { "itinerary.$.meteos_dates" : meteoComponents } },
+        );
+
+        res.status(201).send("Meteo data added to itinerary: "+req.body.itinerary_id+"\nBinded to user: "+req.body.user_id+"\n");
+    
     }catch(err){
-        console.log("Itinerary with id:"+req.body.itinerary_id+" not found");
         res.status(400).send("Itinerary with id:"+req.body.itinerary_id+" not found");
     }
 
@@ -95,46 +53,14 @@ router.post('', async (req, res) => {
 
 router.delete('', async (req,res)=> {
 
-    //console.log("Id ottenuto : "+req.body.id);
-
     try{
+        await User.updateOne(
+            {"_id": req.body.user_id, "itinerary._id" : req.body.itinerary_id},
+            {"$pull" : { "itinerary.$.meteos_dates" : {"_id" : req.body.meteo_id} } }
+        );
 
-        let founduser = await User.findById(req.body.user_id);
-        //console.log("Itinerary with id:"+req.body.id+" found");
-
-        let founditinerary;
-        let foundmeteocomponent;
-        var count1=-1;
-        var count2=-1;
-
-        let infolenIT = Object.keys(founduser.itinerary).length;
-
-        for(let i=0;i<infolenIT;i++){
-            if(founduser.itinerary[i].id == req.body.itinerary_id){
-                founditinerary = founduser.itinerary[i];
-                infolenIT=-1;
-                count1=i;
-            }
-        }
-
-        let infolenMETEO = Object.keys(founditinerary.meteos_dates).length;
-
-        for(let i=0;i<infolenMETEO;i++){
-            if(founditinerary.meteos_dates[i].id == req.body.meteo_id){
-                foundmeteocomponent = founditinerary.meteos_dates[i];
-                infolenMETEO=-1;
-                count2=i;
-            }
-        }
-
-        if(count2!=-1){
-            await User.updateOne(
-                {"_id": req.body.user_id, "itinerary._id" : req.body.itinerary_id},
-                {"$pull" : { "itinerary.$.meteos_dates" : foundmeteocomponent } }
-            );
-            res.status(201).send("MeteoComponent "+req.body.meteo_id+" deleted\nItinerary "+req.body.itinerary_id+" updated.\n");
-        } else
-            res.status(400).send("Meteocomponent "+req.body.meteo_id+" not found.\n");
+        res.status(201).send("MeteoComponent "+req.body.meteo_id+" deleted\nItinerary "+req.body.itinerary_id+" updated.\n");
+    
     }catch(err){
         res.status(400).send("Meteocomponent "+req.body.meteo_id+" not found.\n");
     }
