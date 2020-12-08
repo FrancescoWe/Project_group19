2f193140b153d9ab920afd156dec76a6997996a6
Giacomo Catelan <giacomo.catelan@studenti.unitn.it>
2020-12-08 23:52:11 +0100

    fix ad un import sbagliato in form.js - component di react

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,6 +1,6 @@
 // import { type } from "os";
 import React, { useEffect, useState } from "react"
-import Button from "@material-ui/core/button"
+import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import InputLabel from "@material-ui/core/InputLabel"
 import InputAdornment from '@material-ui/core/InputAdornment'
